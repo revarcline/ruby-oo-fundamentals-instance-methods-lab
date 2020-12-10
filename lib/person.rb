a95b77d58189efe3048a0ec18e86d06be609d23c
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-10 11:31:08 -0600

    commit tree 2c9bdb52a4542274d61945179e1de24ff3c15924

---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class Person
+  # will talk and walk, garbage collection converts to soylent green
+  def talk
+    puts 'Hello World!'
+  end
+
+  def walk
+    puts 'The Person is walking'
+  end
+end
